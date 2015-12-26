be91888010de6212006de0c66015a249bf87d80e
Sinclair Target <sinclairtarget@gmail.com>
2015-12-26 19:51:47 +0800

    Added formula dir and um formula.

---
+++ Formula/um.rb
@@ -0,0 +1,18 @@
+class Um < Formula
+  desc "Command-line utility for creating and maintaining personal man pages"
+  homepage "https://github.com/sinclairtarget/um"
+  url "https://github.com/sinclairtarget/um/archive/1.0.2.tar.gz"
+  version "1.0.2"
+  sha256 "2906a60bf26955b7181eb495bf3512086b241932a195a347ea3869d78b37f8ea"
+
+  bottle :unneeded
+  depends_on :ruby => "2.2.0"
+
+  def install
+    prefix.install Dir.glob("*")
+  end
+
+  test do
+    system "um list"
+  end
+end
