2aa1bc50f4a4d5b01157acb4ce1d0c03161992ee
Sinclair Target <sinclairtarget@gmail.com>
2016-01-16 21:54:31 -0500

    Update um.rb

--- Formula/um.rb
+++ Formula/um.rb
@@ -1,9 +1,9 @@
 class Um < Formula
   desc "Command-line utility for creating and maintaining personal man pages"
   homepage "https://github.com/sinclairtarget/um"
-  url "https://github.com/sinclairtarget/um/archive/1.1.tar.gz"
-  version "1.1"
-  sha256 "4eb4018b4703abac7c859e52ed82c548d4c82398833015c46e1b8f90dff82029"
+  url "https://github.com/sinclairtarget/um/archive/1.2.0.tar.gz"
+  version "1.2.0"
+  sha256 "5bba0146941de567ebaa639258ffb4ac6bd40a11f1bc7b3e395798352c6b3268"
 
   bottle :unneeded
   depends_on :ruby => "2.2.0"
