5761ca33fafc57e8ef850be1a064f18908277aa6
Sinclair Target <sinclairtarget@gmail.com>
2017-09-26 19:19:13 -0400

    Update um formula to 3.0.3.

--- Formula/um.rb
+++ Formula/um.rb
@@ -1,17 +1,19 @@
 class Um < Formula
-  desc "Command-line utility for creating and maintaining personal man pages"
-  homepage "https://github.com/sinclairtarget/um"
-  url "https://github.com/sinclairtarget/um/archive/2.2.1.tar.gz"
-  sha256 "451b9706465691513ca9e50fe9021f1d3a80ffb0a61bf34898a49c90ac88b1a5"
+  desc 'Command-line utility for creating and maintaining personal man pages'
+  homepage 'https://github.com/sinclairtarget/um'
+  url 'https://github.com/sinclairtarget/um/archive/3.0.3.tar.gz'
+  sha256 '467073c5cb187ca320b012d8e54920e7759d9b7622bf035699d86d6c71ec3bd6'
 
   bottle :unneeded
-  depends_on :ruby => "2.2.0"
+  depends_on :ruby => '2.2.0'
+  depends_on 'pandoc'
 
   def install
-    bash_completion.install "um-completion.sh"
+    bash_completion.install 'um-completion.sh'
+    man1.install Dir['doc/man1/*']
 
-    prefix.install Dir.glob("*")
- 
-    File.write("#{share}/version.txt", version)
+    prefix.install Dir['*']
+
+    File.write("#{prefix}/version.txt", version)
   end
 end
